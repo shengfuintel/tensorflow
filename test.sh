845d688178bb441fd9eefbb136fb982e7c83b22b
shengfuintel <sheng.fu@intel.com>
2019-02-14 18:05:20 -0800

    WIP

---
+++ test.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+export TEST_TMPDIR=/dataset/sfu2/cache/sycl
+
+ cp /dataset/sfu2/nervana/git/mkldnn-sycl/mkl-dnn/build/mkldnn_config.h /dataset/sfu2/cache/sycl/_bazel_sfu2/972ddc02f896eef702e3ea03a320b700/external/mkl_dnn/include
+
+
+bazel build --verbose_failures -c opt --config=sycl --config=mkl --cxxopt=-no-serial-memop //tensorflow/tools/pip_package:build_pip_package
+
+
+bazel-bin/tensorflow/tools/pip_package/build_pip_package /dataset/sfu2/nervana/pkg-sycl
+
+pip2 install --upgrade --target=/dataset/sfu2/nervana/install-sycl /dataset/sfu2/nervana/pkg-sycl/
+
+rm /dataset/sfu2/nervana/install-sycl/tensorflow/libtensorflow_framework.so
+
+ln -s /dataset/sfu2/cache/sycl/_bazel_sfu2/972ddc02f896eef702e3ea03a320b700/execroot/org_tensorflow/bazel-out/k8-dbg/bin/tensorflow/libtensorflow_framework.so /dataset/sfu2/nervana/install-sycl/tensorflow/libtensorflow_framework.so
+
+rm /dataset/sfu2/nervana/install-sycl/tensorflow/python/_pywrap_tensorflow_internal.so
+
+ln -s /dataset/sfu2/cache/sycl/_bazel_sfu2/972ddc02f896eef702e3ea03a320b700/execroot/org_tensorflow/bazel-out/k8-dbg/bin/tensorflow/python/_pywrap_tensorflow_internal.so /dataset/sfu2/nervana/install-sycl/tensorflow/python/_pywrap_tensorflow_internal.so 
+
+
+cp /dataset/sfu2/nervana/git/mkldnn-sycl/mkl-dnn/build/mkldnn_config.h /dataset/sfu2/cache/sycl/_bazel_sfu2/972ddc02f896eef702e3ea03a320b700/external/mkl_dnn/include
